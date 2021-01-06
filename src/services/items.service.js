a00f90c80c3a2bc24211e4a848f2a82b7b30382a
maprodri <maprodri@bancolombia.com.co>
2021-01-05 23:36:12 -0500

    increment: Agrega dos librerías y un servicio. 	- Agrega la librería axios y react-hook-form. 	- Agrega el servicio para obtener los items del backend.

---
+++ src/services/items.service.js
@@ -0,0 +1,27 @@
+import { api } from '../common/environment';
+import axios from "axios";
+
+export default class ItemsService {
+  apiEndpoint = api.ANALYSTS_URL;
+
+  /**
+   * Función para obtener la lista de items según una búsqueda determinada
+   * @param {*} q 
+   */
+  getItems = (q) => {
+    console.log(q);
+    return axios.get(`${api.MLBACKEND_URL}/items`, {
+      params: {
+        q
+      }
+    });
+  };
+
+  /**
+   * Función para obtener un item según su id
+   * @param {*} q 
+   */
+  getItem = (id) => {
+    return axios.get(`${api.MLBACKEND_URL}/items/${id}`);
+  };
+}
