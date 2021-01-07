a621364e98528f0dd9e881e547ffa695bf5342e4
maprodri <maprodri@bancolombia.com.co>
2021-01-06 22:52:57 -0500

    increment: Crea la mólecula item. 	- Agrega la librería 'react-currency-format'.

---
+++ src/components/molecules/item/item.jsx
@@ -0,0 +1,31 @@
+import './item.scss';
+
+import CurrencyFormat from 'react-currency-format';
+import FreeShippingImg from '../../../assets/images/shipping18.png';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+const Item = ({ id, imgSrc, price, isFreeshipping, name, city }) => {
+  return (
+    <article className='m-item'>
+      <Link to={`/items/${id}`} className='m-item__img'>
+        <img src={imgSrc} alt={name}></img>
+      </Link>
+      <Link to={`/items/${id}`} className='m-item__detail'>
+        <h2 className='m-item__price'> 
+          <CurrencyFormat 
+            value={price} 
+            displayType={'text'} 
+            thousandSeparator={true} 
+            decimalSeparator='.' 
+            prefix={'$'}/> 
+          { isFreeshipping ? <img src={FreeShippingImg} alt='Envío gratis'></img> : null } 
+        </h2>
+        <h3 className='m-item__name'>{ name }</h3>
+      </Link>
+      <p className='m-item__city'>{city}</p>
+    </article>
+  );
+};
+
+export default Item;
